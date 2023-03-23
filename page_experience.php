2071ece69aa19134b89aa72b12a5a00357f66249
Julien <lissjulieng@mail.com>
2023-03-23 10:14:37 +0100

    First commit for organized files

---
+++ page_experience.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/assets/css/style.css">
+  <title>CV Iron Man</title>
+</head>
+
+<body>
+  <?php include __DIR__ . '/include/navbar.php' ?>
+  <main>
+    <header class="titre_experience">
+      <h1>Experiences</h1>
+    </header>
+
+    <section>
+      <div class="timeline">
+        <div class="outer">
+          <div class="card">
+            <div class="info">
+              <h3 class="title">Title 1</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="info">
+              <h3 class="title">Title 2</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="info">
+              <h3 class="title">Title 3</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="info">
+              <h3 class="title">Title 4</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. </p>
+            </div>
+          </div>
+          <div class="card">
+            <div class="info">
+              <h3 class="title">Title 5</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <?php include __DIR__ . '/include/footer.php' ?>
+    </footer>
+  </main>
+</body>
